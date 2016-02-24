45369425eb571e7ec60bf7563fc0a271bc072228
Shrey Naygandhi <shreynaygandhi@live.com>
2016-02-25 00:09:49 +0530

    prac 4.21

---
+++ Prac4/4.21/ServletUsingHtml.java
@@ -0,0 +1,84 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletInsertingDataUsingHtml
+ */
+@WebServlet("/ServletUsingHtml")
+public class ServletUsingHtml extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	static final String JDBC_DRIVER = "org.postgresql.Driver";
+	static final String DB_URL = "jdbc:postgresql://127.0.0.1:5433/s2c130050131527";
+
+	static final String USER = "postgres";
+	static final String PASS = "shrey97266";
+
+	Connection Conn;
+	Statement stmt;
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		
+		try {
+			Class.forName(JDBC_DRIVER);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			Conn=DriverManager.getConnection(DB_URL, USER, PASS);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			stmt=Conn.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String id1=request.getParameter("id");
+		String name=request.getParameter("name");
+		String branch=request.getParameter("branch");
+		int id=Integer.parseInt(id1);
+		String sql="INSERT INTO registration VALUES("+id+",'"+name+"','"+branch+"')";
+		try {
+			stmt.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		out.println("Enroll no: 130050131527");
+		
+
+		out.println("Values Inserted Successfuly");
+	
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
